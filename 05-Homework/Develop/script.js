e17109ab6569e5b47c1a46a9371a19fb6bf930a8
aod1411 <87099278+aod1411@users.noreply.github.com>
2021-08-25 21:45:06 -0400

    set up rows and local storage for each row

---
+++ 05-Homework/Develop/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+    $('#currentDay').text(moment().format("dddd, MMMM Do"))
+$(".saveBtn").on('click', function(event){
+    event.preventDefault();
+
+var userText = $(this).siblings(".description").val();
+var time = $(this).parent().attr('id');
+
+localStorage.setItem(time, userText);
+
+$('.notification').addClass("show")
+
+setTimeout(function(){
+    $('.notification').removeClass("show")
+}, 5000)
+})
+
+function keepCurrentTime(){
+var currentHour = moment().hours();
+console.log(currentHour)
+
+
+}
+
+keepCurrentTime();
+
+$("#hour-9 .description").val(localStorage.getItem('hour-9'))
+$("#hour-10 .description").val(localStorage.getItem('hour-10'))
+$("#hour-11 .description").val(localStorage.getItem('hour-11'))
+$("#hour-12 .description").val(localStorage.getItem('hour-12'))
+$("#hour-13 .description").val(localStorage.getItem('hour-13'))
+$("#hour-14 .description").val(localStorage.getItem('hour-14'))
+$("#hour-15 .description").val(localStorage.getItem('hour-15'))
+$("#hour-16 .description").val(localStorage.getItem('hour-16'))
+$("#hour-17 .description").val(localStorage.getItem('hour-17'))
+})
